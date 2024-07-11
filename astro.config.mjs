7881a143668647ce1a836a7a31f9002cba6929a1
MarioA96 <marioaquirozs@gmail.com>
2024-07-10 22:06:03 -0600

    actualizaciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://marioa96.github.io'
+    site: 'https://marioa96.github.io',
+    base: '05_FEM_newslatterSignUp'
 });
